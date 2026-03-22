d60c397285be09462b7e22634a4cb3a026ffe2cb
Vedant Sharma <vedants201011@gmail.com>
2026-03-22 17:05:53 -0400

    oiuyhgf

--- Services/core/urls.py
+++ Services/core/urls.py
@@ -13,4 +13,6 @@ urlpatterns = [
     path('invoices/add/', views.add_invoice, name='add_invoice'),
     path('invoices/<int:invoice_id>/', views.view_invoice, name='view_invoice'),
     path('appointments/', views.appointments, name='appointments'),
+    path('appointments/add/', views.add_appointment, name='add_appointment'),
+    path('appointments/<int:appointment_id>/', views.view_appointment, name='view_appointment'),
     ]
