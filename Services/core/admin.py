c8267c208f9e4abed2b6d5effdb1ec85e01c090e
Vedant Sharma <vedants201011@gmail.com>
2026-03-22 12:36:49 -0400

    update

--- Services/core/admin.py
+++ Services/core/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from .models import Customer, Client
 
+class ClientInline(admin.TabularInline):
+    model = Client
+    extra = 0
+    fields = ['name', 'email', 'phone']
+
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ['firm_name', 'contact_name', 'email', 'last_paid_at', 'key_expires_at', 'is_active']
     list_filter = ['key_expires_at']
     search_fields = ['firm_name', 'email']
+    inlines = [ClientInline]
 
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
